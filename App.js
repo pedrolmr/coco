02e12b18bef55ae916a584ce7023e1029417b23c
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-09-14 22:53:38 -0400

    created card component

--- App.js
+++ App.js
@@ -3,23 +3,25 @@ import { StyleSheet, SafeAreaView, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 
 export default function App() {
   // return <WelcomeScreen />;
   // return <ViewImageScreen />;
   return (
-    // <View
-    //   style={{
-    //     flex: 1,
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //   }}
-    // >
-    //   <AppButton
-    //     title="Login"
-    //     onPress={() => console.log("login button tapped!!")}
-    //   />
-    // </View>
-    <WelcomeScreen />
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="makeup for sale"
+        subTitle="$30"
+        image={require("./app/assets/makeup.jpg")}
+      />
+    </View>
+    // <WelcomeScreen />
   );
 }
