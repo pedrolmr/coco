33d0bb9e57db2ce0fde60dec2526dd9203b29c5d
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-09-27 22:04:24 -0400

    built account screen

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
   // return <WelcomeScreen />;
@@ -27,6 +31,7 @@ export default function App() {
     // <ListingDetailsScreen />
     // <WelcomeScreen />
     // <ViewImageScreen />
-    <MessagesScreen />
+    // <MessagesScreen />
+    <AccountScreen />
   );
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -15,6 +15,7 @@ export default function ListItem({
   title,
   subTitle,
   image,
+  IconComponent,
   onPress,
   renderRightActions,
 }) {
@@ -22,10 +23,11 @@ export default function ListItem({
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View>
+          {IconComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -37,12 +39,17 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
+    // marginRight: 10,
   },
   subTitle: {
     color: colors.medium,
